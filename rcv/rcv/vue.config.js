a80b0d18b9f35ad1e57d7aeace6e639649222c51
Stephen Bush <darkshark_007@yahoo.com>
2021-04-08 08:24:12 -0600

    master Add Vuetify

--- rcv/rcv/vue.config.js
+++ rcv/rcv/vue.config.js
@@ -1,11 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    publicPath: '/static/src/vue/dist/', // Should be STATIC_URL + path/to/build
-    outputDir: path.resolve(__dirname, '../static/src/vue/dist/'), // Output to a directory in STATICFILES_DIRS
-    filenameHashing: false, // Django will hash file names, not webpack
-    runtimeCompiler: true, // See: https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
+    // Should be STATIC_URL + path/to/build
+    publicPath: '/static/src/vue/dist/',
+
+    // Output to a directory in STATICFILES_DIRS
+    outputDir: path.resolve(__dirname, '../static/src/vue/dist/'),
+
+    // Django will hash file names, not webpack
+    filenameHashing: false,
+
+    // See: https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
+    runtimeCompiler: true,
+
     devServer: {
         writeToDisk: true, // Write files to disk in dev mode, so Django can serve the assets
     },
+
+    transpileDependencies: [
+      'vuetify'
+    ]
 };
