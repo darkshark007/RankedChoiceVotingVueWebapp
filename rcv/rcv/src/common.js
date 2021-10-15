d5ebeb946613b9e6428560f15d5be5b38e8007eb
Stephen Bush <darkshark_007@yahoo.com>
2021-10-14 20:50:58 -0600

    master Fix broken list

--- rcv/rcv/src/common.js
+++ rcv/rcv/src/common.js
@@ -105,6 +105,7 @@ export default {
             choices: [],
             ballots: [],
             ballotsPublic: [],
+            oldPolls: [],
             pollRoute: '',
             editRoute: '',
             editBallots: '',
